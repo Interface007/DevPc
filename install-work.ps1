ff48e1cf685a9e73278291fd2eca6e4bdf517937
Sven Erik Matzen <sven@matzen.cloud>
2023-03-23 20:36:05 +0100

    Update install-work.ps1

--- install-work.ps1
+++ install-work.ps1
@@ -60,93 +60,75 @@ Install-Font "https://github.com/google/fonts/raw/main/ofl/redactedscript" "Reda
 Install-Font "https://github.com/google/fonts/raw/main/ofl/redactedscript" "RedactedScript-Light.ttf"
 Install-Font "https://github.com/google/fonts/raw/main/ofl/redactedscript" "RedactedScript-Regular.ttf"
 
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-# we don't want to manually confirm each and every installation
-choco feature enable -n allowGlobalConfirmation
-
 # scheduling daily upgrades of all software
-$existingTask = Get-ScheduledTask -TaskName "Choco Upgrade All" -ErrorAction Ignore -WarningAction Ignore
-if (!$existingTask) {
-    $action = New-ScheduledTaskAction `
-        -Execute 'C:\ProgramData\chocolatey\bin\choco.exe' `
-        -Argument 'upgrade all -y'
+#$existingTask = Get-ScheduledTask -TaskName "WinGet Upgrade --All" -ErrorAction Ignore -WarningAction Ignore
+#if (!$existingTask) {
+#    $action = New-ScheduledTaskAction `
+#        -Execute 'C:\ProgramData\chocolatey\bin\choco.exe' `
+#        -Argument 'upgrade all -y'
+#
+#    $trigger =  New-ScheduledTaskTrigger -Daily -At 12am
+#    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Choco Upgrade All" -Description "Upgrade all choco packages"   
+#}  else {
+#    Write-Host "task for auto-update exists"
+#}
 
-    $trigger =  New-ScheduledTaskTrigger -Daily -At 12am
-    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Choco Upgrade All" -Description "Upgrade all choco packages"   
-}  else {
-    Write-Host "task for auto-update exists"
-}
 
+#winget install firacode                          # (free) monospaced font with programming ligatures
+winget install lightshot                          # (free) screenshots the way I want them to be
+winget install vlc                                # (free) the(!) video player
+winget install gimp.gimp                          # (free) image editing
+winget install adobereader                        # (feee) PDF reader
+winget install 7zip                               # (free) handles most comression file formats
+winget install treesizefree                       # (free) analyzes where all the harddisk space has gone
+winget install everything                         # (free) filename search
+winget install fiddler                            # (free) debugging proxy for http(s)
 
-choco install firacode                          # (free) monospaced font with programming ligatures
-choco install lightshot                         # (free) screenshots the way I want them to be
-choco install vlc                               # (free) the(!) video player
-choco install gimp                              # (free) image editing
-choco install adobereader                       # (feee) PDF reader
-choco install 7zip                              # (free) handles most comression file formats
-choco install treesizefree                      # (free) analyzes where all the harddisk space has gone
-choco install microsoft-edge                    # (free) browser based on Chromium
-choco install everything                        # (free) filename search
-choco install fiddler                           # (free) debugging proxy for http(s)
+winget install --id=Obsidian.Obsidian  -e         # (freemium) "external brain"
+winget install tailscale                          # (freemium) point-to-point-VPN
 
-choco install tailscale                         # (freemium) point-to-point-VPN
-
-choco install xmind-2020                        # (paid) mindmapping
-choco install beyondcompare                     # (paid) takes comparison of folders and files to a new level
-choco install cryptomator                       # (free) need to keep some content secret in the cloud
+winget install xmind-2020                         # (paid) mindmapping
+winget install beyondcompare                      # (paid) takes comparison of folders and files to a new level
+winget install cryptomator                        # (free) need to keep some content secret in the cloud
 
 # software development and business
-choco install git                               # version control
+winget install git                               # version control
 
-choco install python3                           # (free) Python 3 programming language
-choco install dotnet-5.0-runtime                # .NET runtime
-choco install dotnet-aspnetruntime              # .NET ASP runtime
-choco install dotnet-sdk                        # .NET SDK
+winget install dotnet-sdk                        # .NET SDK
 
-choco install vscode                            # Visual Studio Code
-choco install visualstudio2022enterprise        # Visual Studio 2022 Enterprise - still need to install components via VS installer GUI
-choco install resharper-ultimate-all            # tooling for efficient C# coding - might be obsolete in the future, but currently it has a lot of great efficiency improvements
+winget install vscode                            # Visual Studio Code
+winget install visualstudio2022enterprise        # Visual Studio 2022 Enterprise - still need to install components via VS installer GUI
+winget install resharper-ultimate-all            # tooling for efficient C# coding - might be obsolete in the future, but currently it has a lot of great efficiency improvements
 
-choco install azure-cli                         # tooling for Azure
-choco install azure-functions-core-tools        # tooling for AzureFunctions
-choco install azcopy10                          # Azure Storage copy 
-choco install bicep                             # Azure IaC-Tooling for Bicep-definitions
+winget install azure-cli                         # tooling for Azure
+winget install azure-functions-core-tools        # tooling for AzureFunctions
+winget install azcopy10                          # Azure Storage copy 
+winget install bicep                             # Azure IaC-Tooling for Bicep-definitions
 
-choco install sql-server-management-studio      # database development with MS SQL Server 
-choco install azure-data-studio                 # database development with MS Azure Studio
-choco install sql-server-2019-cumulative-update # update to latest sql server CU
-choco install sqlserver-odbcdriver              # SQL Server Driver (ODBC)
-choco install msoledbsql                        # SQL Server Driver (OLEDB)
-choco install sql2012.nativeclient              # SQL Server Driver (native client 2012)
-choco install mysql.workbench                   # database development with MySQL Server 
-choco install mysql-cli                         # MySQL Command Line Utility
+winget install sql-server-management-studio      # database development with MS SQL Server 
+winget install azure-data-studio                 # database development with MS Azure Studio
 
-choco install rdmfree                           # (free) Remote Desktop Manager 
-choco install lastpass                          # keep the secrets secret
-choco install postman                           # (free) create and manage REST-API-calls
-choco install microsoft-teams.install           # (subs) will "install the installer" - user has to install
-choco install microsoft-teams                   # (subs) will install for current user
-choco install googlechrome                      # (free) browser based on Chromium
-choco install firefox                           # (free) browser based on Gecko
-choco install kindle                            # (free) to read your programming books from Amazon
-choco install onedrive                          # OneDrive
-choco install zoomit                            # (free) ZoomIt tool from SysInternals
+winget install lastpass                          # keep the secrets secret
+winget install postman                           # (free) create and manage REST-API-calls
+winget install googlechrome                      # (free) browser based on Chromium
+winget install firefox                           # (free) browser based on Gecko
+winget install kindle                            # (free) to read your programming books from Amazon
+winget install zoomit                            # (free) ZoomIt tool from SysInternals
 
-choco install nodejs                            # (free) Node.js - open-source, cross-platform, back-end JavaScript runtime environment
-choco install powershell-core                   # (free) open shouce shell
-choco install az.powershell                     # Azure PowerShell
-choco install docker-desktop                    # Docker Desktop
+winget install nodejs                            # (free) Node.js - open-source, cross-platform, back-end JavaScript runtime environment
+winget install powershell-core                   # (free) open shouce shell
+winget install az.powershell                     # Azure PowerShell
+winget install docker-desktop                    # Docker Desktop
 
-choco install citrix-workspace                  # (free) needed for some remote work scenarios
-choco install vmware-horizon-client             # (free) some use this desktop virtualization
-choco install ilspy                             # (free) Intermediate Language (.NET) Disassembler
+winget install citrix-workspace                  # (free) needed for some remote work scenarios
+winget install vmware-horizon-client             # (free) some use this desktop virtualization
+winget install ilspy                             # (free) Intermediate Language (.NET) Disassembler
 
-choco install vcredist140                       # (free) dependency for some software - installed just to be able to upgrade
-choco install vcredist2015                      # (free) dependency for some software - installed just to be able to upgrade
+winget install vcredist140                       # (free) dependency for some software - installed just to be able to upgrade
+winget install vcredist2015                      # (free) dependency for some software - installed just to be able to upgrade
 
-choco install screamingfrog                     # (paid) website SEO spider
+winget install screamingfrog                     # (paid) website SEO spider
 
-choco install obs-studio                        # to record screen/cam/...
+winget install obs-studio                        # to record screen/cam/...
 
-choco install spotify                           # (free) need good music - this installer sometimes hangs at the end of the procedure - so I put it last
+winget install spotify                           # (free) need good music - this installer sometimes hangs at the end of the procedure - so I put it last
